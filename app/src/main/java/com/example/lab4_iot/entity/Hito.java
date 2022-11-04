3f8d87f2a526a76f452ad05b2c7c25458d9490a0
Diego Vargas <dvargasr@pucp.edu.pe>
2022-11-04 09:46:46 -0500

    Configuración de hito

---
+++ app/src/main/java/com/example/lab4_iot/entity/Hito.java
@@ -0,0 +1,40 @@
+package com.example.lab4_iot.entity;
+
+public class Hito {
+    private String equipo;
+    private String nombre;
+    private String apellido;
+    private String hito;
+
+    public String getEquipo() {
+        return equipo;
+    }
+
+    public void setEquipo(String equipo) {
+        this.equipo = equipo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getHito() {
+        return hito;
+    }
+
+    public void setHito(String hito) {
+        this.hito = hito;
+    }
+}
