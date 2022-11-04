bc29a600b78bc63fdd72d116f2a8a0e23a59cd93
victordzx <diaz.victorm@pucp.edu.pe>
2022-11-04 09:57:31 -0500

    Vistas de usuario

---
+++ app/src/main/java/com/example/lab4_iot/Activity_UserOptions.java
@@ -0,0 +1,14 @@
+package com.example.lab4_iot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_UserOptions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_options);
+    }
+}
