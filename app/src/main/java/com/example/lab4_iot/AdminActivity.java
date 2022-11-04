0cf51865f752d6f8038ca2f22986e022dac529a2
Leonardo Ayala <leonardo.ayalao@pucp.edu.pe>
2022-11-04 09:43:04 -0500

    AdminActivity codigo

--- app/src/main/java/com/example/lab4_iot/AdminActivity.java
+++ app/src/main/java/com/example/lab4_iot/AdminActivity.java
@@ -3,6 +3,8 @@ package com.example.lab4_iot;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class AdminActivity extends AppCompatActivity {
 
@@ -10,6 +12,22 @@ public class AdminActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
+
+        String[] equipos = {"Alianza Lima","Universitario de Deportes"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item,equipos);
+        Spinner spinnerEquipos = findViewById(R.id.spinnerEquipo);
+        spinnerEquipos.setAdapter(adapter);
+
+        String[] nombres = {"Luis","Pedro"};
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item,nombres);
+        Spinner spinnerNombre = findViewById(R.id.spinnerNombre);
+        spinnerNombre.setAdapter(adapter1);
+
+        String[] apellidos = {"Diaz","Perez"};
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item,apellidos);
+        Spinner spinnerApellido = findViewById(R.id.spinnerApellido);
+        spinnerApellido.setAdapter(adapter2);
+
     }
 
 
