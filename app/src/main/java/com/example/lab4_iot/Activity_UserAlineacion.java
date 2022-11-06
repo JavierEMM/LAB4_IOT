d83f3e206c3a58872dcee510ee612c2eede722a9
Oliver <oliver.bustamante@pucp.edu.pe>
2022-11-05 21:16:39 -0500

    Terminación de las vistas de usuario, falta backend

--- app/src/main/java/com/example/lab4_iot/Activity_UserAlineacion.java
+++ app/src/main/java/com/example/lab4_iot/Activity_UserAlineacion.java
@@ -3,6 +3,7 @@ package com.example.lab4_iot;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class Activity_UserAlineacion extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class Activity_UserAlineacion extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_alineacion);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_user_alineacion,menu);
+        return true;
     }
 }
---
+++ app/src/main/java/com/example/lab4_iot/adapters/listaHitosAdapter.java
@@ -0,0 +1,55 @@
+package com.example.lab4_iot.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.lab4_iot.R;
+import com.example.lab4_iot.entity.Hito;
+
+import java.util.ArrayList;
+
+public class listaHitosAdapter extends RecyclerView.Adapter<listaHitosAdapter.hitoViewHolder> {
+
+    private ArrayList<Hito> listaHitos;
+    private Context context;
+
+    public class hitoViewHolder extends RecyclerView.ViewHolder {
+        Hito hito;
+
+        public hitoViewHolder(@NonNull View itemView){
+            super(itemView);
+        }
+    }
+
+
+    @NonNull
+    @Override
+    public hitoViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(context).inflate(R.layout.recycle_hitos,parent,false);
+        return new hitoViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull hitoViewHolder holder, int position) {
+        Hito h = listaHitos.get(position);
+        TextView textJugador =holder.itemView.findViewById(R.id.textJugador);
+        TextView textHito = holder.itemView.findViewById(R.id.textHito);
+        String sJugador = "Jugador: "+ h.getNombre()+" " +h.getApellido()+"\nEquipo: "+h.getEquipo();
+        String sHito = h.getHito();
+
+        textJugador.setText(sJugador);
+        textHito.setText(sHito);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return listaHitos.size();
+    }
+}
