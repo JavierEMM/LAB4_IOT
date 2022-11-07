db0bc5c5dfaa0577c4972fa4c2f1bdcbcbe37a8e
Javier Marroquin <a20193644@pucp.edu.pe>
2022-11-06 23:47:27 -0500

    Admin últimas modificaciones

--- app/src/main/java/com/example/lab4_iot/AdminActivity.java
+++ app/src/main/java/com/example/lab4_iot/AdminActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -21,6 +22,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 
 public class AdminActivity extends AppCompatActivity {
@@ -28,52 +30,95 @@ public class AdminActivity extends AppCompatActivity {
     FirebaseDatabase firebaseDatabase;
     ArrayList<String> listaNombres = new ArrayList<>();
     ArrayList<String> listaApellidos = new ArrayList<>();
+    ArrayList<Jugador> listaJugadores = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
-
         firebaseDatabase = FirebaseDatabase.getInstance();
 
-        String[] equipos = {"Alianza Lima","Universitario de Deportes"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item,equipos);
+        String[] equipos = {"Alianza Lima", "Universitario de Deportes"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, equipos);
         Spinner spinnerEquipos = findViewById(R.id.spinnerEquipo);
+        spinnerEquipos.setSelection(0);
         spinnerEquipos.setAdapter(adapter);
+
 
         DatabaseReference databaseReference = firebaseDatabase.getReference().child("Jugadores");
         databaseReference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
+                snapshot.getChildren().iterator();
                 for(DataSnapshot children : snapshot.getChildren() ){
                     Jugador jugador = children.getValue(Jugador.class);
-                    listaNombres.add(jugador.getNombre());
-                    listaApellidos.add(jugador.getApellido());
-                    ArrayList<String> jugadores = listaNombres;
-                    ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(AdminActivity.this, android.R.layout.simple_spinner_dropdown_item,jugadores);
-                    Spinner spinnerNombre = findViewById(R.id.spinnerNombre);
-                    spinnerNombre.setAdapter(adapter1);
-                    spinnerNombre.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-                        @Override
-                        public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                            ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(AdminActivity.this, android.R.layout.simple_spinner_dropdown_item, Collections.singletonList(listaApellidos.get(i)));
-                            Spinner spinnerApellido = findViewById(R.id.spinnerApellido);
-                            spinnerApellido.setEnabled(false);
-                            spinnerApellido.setAdapter(adapter2);
-                        }
-                        @Override
-                        public void onNothingSelected(AdapterView<?> adapterView) {
-
-                        }
-                    });
-
+                    listaJugadores.add(jugador);
                 }
-            }
+                listaNombres = new ArrayList<>();
+                listaApellidos = new ArrayList<>();
+                for(Jugador jugador : listaJugadores){
+                    if(jugador.getEquipo().equals("Alianza Lima")){
+                        listaNombres.add(jugador.getNombre());
+                        listaApellidos.add(jugador.getApellido());
+                    }
+                }
+                ArrayList<String> jugadores = listaNombres;
+                ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(AdminActivity.this, android.R.layout.simple_spinner_dropdown_item,jugadores);
+                Spinner spinnerNombre = findViewById(R.id.spinnerNombre);
+                spinnerNombre.setAdapter(adapter1);
+                spinnerNombre.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(AdminActivity.this, android.R.layout.simple_spinner_dropdown_item, Collections.singletonList(listaApellidos.get(i)));
+                        Spinner spinnerApellido = findViewById(R.id.spinnerApellido);
+                        spinnerApellido.setEnabled(false);
+                        spinnerApellido.setAdapter(adapter2);
+                    }
+                    @Override
+                    public void onNothingSelected(AdapterView<?> adapterView) {
 
+                    }
+                });
+            }
             @Override
             public void onCancelled(@NonNull DatabaseError error) {}
         });
+        spinnerEquipos.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                listaNombres = new ArrayList<>();
+                listaApellidos = new ArrayList<>();
+                String equipo = spinnerEquipos.getSelectedItem().toString();
+                for(Jugador jugador : listaJugadores){
+                    if(jugador.getEquipo().equals(equipo)){
+                        listaNombres.add(jugador.getNombre());
+                        listaApellidos.add(jugador.getApellido());
+                    }
+                }
+                ArrayList<String> jugadores = listaNombres;
+                ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(AdminActivity.this, android.R.layout.simple_spinner_dropdown_item,jugadores);
+                Spinner spinnerNombre = findViewById(R.id.spinnerNombre);
+                spinnerNombre.setAdapter(adapter1);
+                spinnerNombre.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(AdminActivity.this, android.R.layout.simple_spinner_dropdown_item, Collections.singletonList(listaApellidos.get(i)));
+                        Spinner spinnerApellido = findViewById(R.id.spinnerApellido);
+                        spinnerApellido.setEnabled(false);
+                        spinnerApellido.setAdapter(adapter2);
+                    }
+                    @Override
+                    public void onNothingSelected(AdapterView<?> adapterView) {
 
+                    }
+                });
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
 
 
         Button button = findViewById(R.id.btnAdmin);
